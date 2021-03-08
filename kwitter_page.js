d251fac46afcb7a517442cf3f4b808076c831b45
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-03-08 09:50:44 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -14,11 +14,26 @@ var firebaseConfig = {
   function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
    //Start code
-   row = "<div class = 'roomname' id = " + Room_names + " onclick = 'redirectToRoomName(this.id)'>#" + Room_names + "</div>"
-   document.getElementById("Output").innerHTML += row;
+   Un = md['name'];
+    message = md['message'];
+    likes = md['likes'];
+    nwt = "<h4>" + Un + "<img class = 'user_tick' src = 'tick.png'></h4>";
+    mwt = "<h4 class = 'message_h4'>" + message + "</h4>";
+    bwt = "<button class = 'btn btn-warning' id = " + fid + " value = " + likes + " onclick = 'Ul(this.id)'>";
+    swt = "<span class = 'glyphicon glyphicon-thumbs-up'>Likes: " + likes + "</span></button><hr>";
+    row = nwt+mwt+bwt+swt;
+    document.getElementById("Output").innerHTML += row;
    //End code
    });});}
    getData();
+   function Ul(mid){
+    bid = mid;
+    likes = document.getElementById("bid").value;
+    Nl = Number(likes) + 1;
+    firebase.database().ref(room_name).child(md).update({
+     likes:Nl
+ });
+}
    function LogOut(){
     window.location = "kwitter_room.html";
 }
